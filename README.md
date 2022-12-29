2ce2266727fdb8f2f36b316b5721329a78f48d13
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-12-30 00:09:06 +0530

    completed project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Namastey React Live
+
+## Chapter-1: Inception
+
+Things studied:
+
+1. How to add element through vanilla JS.
+1. How to add react to the project using CDN.
+1. How to add elements through React.
+1. What is the meaning of different CDN links?
+1. Meaning of crossorigin attribute in script tag?
+1. Async and Defer
+1. Basics about React.createElement
+
+Detailed notes can be found [here](https://jagrutsharma.notion.site/Namastey-React-4a4a20eb2f90435caf085dbe34c2f944)
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const heading1 = React.createElement(
   {
     id: "title",
   },
-  "Heading 1"
+  "Namastey"
 );
 
 const heading2 = React.createElement(
@@ -11,12 +11,14 @@ const heading2 = React.createElement(
   {
     id: "title",
   },
-  "Heading 2"
+  "React"
 );
 
 const container = React.createElement("div", {}, [heading1, heading2]);
+// multiple elements are passed as array elements
 
 // console.log(heading); // React Element => Object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// *? Used ReactDOM because DOM needs to be manipulated i.e. elements to be added there => the root will be created => the place where I want my react to run
 root.render(container);
